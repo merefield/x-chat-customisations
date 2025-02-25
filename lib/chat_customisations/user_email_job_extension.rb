88930fb0bb5e463788f31cbed00268cb2d68a5d8
merefield <merefield@gmail.com>
2025-02-25 18:17:21 +0000

    add user email to logged info

--- lib/chat_customisations/user_email_job_extension.rb
+++ lib/chat_customisations/user_email_job_extension.rb
@@ -3,7 +3,8 @@ module ChatCustomisations
   module UserEmailJobExtension
     def send_user_email(args)
       ##TODO remove this temporary logging
-      message = "Chat Customisations: send_user_email called with args: #{args} on #{ENV["DISCOURSE_HOSTNAME"]}"
+      user = User.find_by(id: args[:user_id])
+      message = "Chat Customisations: send_user_email called with args: #{args} and user email: #{user.primary_email.email} on #{ENV["DISCOURSE_HOSTNAME"]}"
       Rails.logger.warn("#{message}")
 
       if args[:type] == "chat_summary" && !SiteSetting.x_chat_customisations_chat_summary_emails_enabled
