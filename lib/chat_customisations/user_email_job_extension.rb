d62c7c24aaa0781ab3c5196b08f945348db3d1eb
merefield <merefield@gmail.com>
2025-02-26 08:27:26 +0000

    Add more logging output

--- lib/chat_customisations/user_email_job_extension.rb
+++ lib/chat_customisations/user_email_job_extension.rb
@@ -4,10 +4,13 @@ module ChatCustomisations
     def send_user_email(args)
       ##TODO remove this temporary logging
       user = User.find_by(id: args[:user_id])
-      message = "Chat Customisations: send_user_email called with args: #{args} and user email: #{user.primary_email.email} on #{ENV["DISCOURSE_HOSTNAME"]}"
+      to_address =
+          args[:to_address].presence || user&.primary_email&.email.presence || "no_email_found"
+      message = "Chat Customisations: send_user_email called with args: #{args}, user email: #{user.primary_email.email}, to_address: #{to_address} on #{ENV["DISCOURSE_HOSTNAME"]}"
       Rails.logger.warn("#{message}")
 
       if args[:type] == "chat_summary" && !SiteSetting.x_chat_customisations_chat_summary_emails_enabled
+        set_skip_context(args[:type], args[:user_id], to_address, nil)
         return skip(SkippedEmailLog.reason_types[:custom])
       end
 
