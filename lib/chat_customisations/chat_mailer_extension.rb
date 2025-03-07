54d9fcd53358a2aec4dab890d26ab8a8d73c200d
merefield <merefield@gmail.com>
2025-03-07 12:53:04 +0000

    FIX: unread dms fix - sync code with tests passed fix

--- lib/chat_customisations/chat_mailer_extension.rb
+++ lib/chat_customisations/chat_mailer_extension.rb
@@ -54,12 +54,20 @@ module ChatCustomisations
         ), unread_dms AS (
           SELECT DISTINCT uccm.user_id
           FROM user_chat_channel_memberships uccm
-          JOIN chat_channels cc ON cc.id = uccm.chat_channel_id AND cc.deleted_at IS NULL AND cc.chatable_type = 'DirectMessage'
-          JOIN channel_messages cm ON cm.chat_channel_id = cc.id AND cm.sender_id <> uccm.user_id
-          JOIN eligible_users eu ON eu.id = uccm.user_id AND eu.allow_private_messages
+          JOIN eligible_users eu ON eu.id = uccm.user_id
+            AND eu.allow_private_messages
+          JOIN chat_messages cm ON cm.chat_channel_id = uccm.chat_channel_id
+            AND cm.deleted_at IS NULL
+            AND (cm.thread_id IS NULL OR cm.thread_id IN (SELECT id FROM chat_threads WHERE original_message_id = cm.id))
+            AND NOT cm.created_by_sdk
+            AND cm.created_at > now() - interval '1 day'
+          JOIN users sender ON sender.id = cm.user_id
+          JOIN chat_channels cc ON cc.id = cm.chat_channel_id
+            AND cc.deleted_at IS NULL
+            AND cc.chatable_type = 'DirectMessage'
           WHERE NOT uccm.muted
-          AND (uccm.last_read_message_id IS NULL OR cm.first_unread_id > uccm.last_read_message_id)
-          AND (uccm.last_unread_mention_when_emailed_id IS NULL OR cm.first_unread_id > uccm.last_unread_mention_when_emailed_id)
+            AND (uccm.last_read_message_id IS NULL OR uccm.last_read_message_id < cm.id)
+            AND (uccm.last_unread_mention_when_emailed_id IS NULL OR uccm.last_unread_mention_when_emailed_id < cm.id)
         ), unread_mentions AS (
           SELECT DISTINCT n.user_id
           FROM notifications n
