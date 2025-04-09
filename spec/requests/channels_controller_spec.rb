096c64d9d0ba4d0c776cba1fcd25378a7aab0198
merefield <merefield@gmail.com>
2025-04-09 16:56:23 +0100

    add api tests

---
+++ spec/requests/channels_controller_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+RSpec.describe Chat::Api::ChannelsController do
+  before do
+    SiteSetting.chat_enabled = true
+    SiteSetting.chat_allowed_groups = Group::AUTO_GROUPS[:everyone]
+  end
+
+  describe "#create" do
+    fab!(:admin)
+    fab!(:moderator)
+    fab!(:public_category) { Fabricate(:category, name: "Public Category") }
+    fab!(:dummy_private_category) { Fabricate(:category, name: "Dummy Private Category") }
+
+    let(:public_params) do
+      {
+        channel: {
+          type: public_category.class.name,
+          chatable_id: public_category.id,
+          name: "channel name",
+          slug: "channel-name",
+          description: "My new channel",
+          threading_enabled: true,
+        },
+      }
+    end
+
+    let(:private_params) do
+      {
+        channel: {
+          type: dummy_private_category.class.name,
+          chatable_id: dummy_private_category.id,
+          name: "whatsup",
+          slug: "whatsup",
+          description: "My new private channel",
+          threading_enabled: true,
+        },
+      }
+    end
+
+    before do
+      SiteSetting.x_chat_customisations_private_chat_dummy_category_id = dummy_private_category.id
+      sign_in(admin)
+    end
+
+    it "creates a public channel associated to a category" do
+      post "/chat/api/channels", params: public_params
+      expect(response.status).to eq(200)
+
+      new_channel = Chat::Channel.find(response.parsed_body.dig("channel", "id"))
+
+      expect(new_channel.name).to eq(public_params[:channel][:name])
+      expect(new_channel.slug).to eq("channel-name")
+      expect(new_channel.description).to eq(public_params[:channel][:description])
+      expect(new_channel.chatable_type).to eq(public_category.class.name)
+      expect(new_channel.chatable_id).to eq(public_category.id)
+    end
+
+
+    it "creates a private channel associated to a new category and group, which are removed along with the channel on delete" do
+      post "/chat/api/channels", params: private_params
+      expect(response.status).to eq(200)
+
+      expect(Category.find_by(name: private_params[:channel][:name])).to be_present
+      expect(Category.find_by(name: private_params[:channel][:name]).read_restricted).to eq(true)
+      expect(Group.find_by(name: private_params[:channel][:name])).to be_present
+
+
+      new_channel = Chat::Channel.find(response.parsed_body.dig("channel", "id"))
+
+      expect(new_channel.name).to eq(private_params[:channel][:name])
+      expect(new_channel.slug).to eq("whatsup")
+      expect(new_channel.description).to eq(private_params[:channel][:description])
+      expect(new_channel.chatable_type).to eq(dummy_private_category.class.name)
+      expect(new_channel.chatable_id).to eq(Category.find_by(name: private_params[:channel][:name]).id)
+
+      delete "/chat/api/channels/#{new_channel.id}"
+      expect(response.status).to eq(200)
+      expect(job_enqueued?(job: Jobs::Chat::ChannelDelete, args: { chat_channel_id: new_channel.id, channel_name: new_channel.name }),).to eq(true)
+      Jobs::Chat::ChannelDelete.new.execute(chat_channel_id: new_channel.id, channel_name: new_channel.name)
+
+      expect(Category.find_by(name: private_params[:channel][:name])).not_to be_present
+      expect(Group.find_by(name: private_params[:channel][:name])).not_to be_present
+      expect(Chat::Channel.find_by(id: new_channel.id)).not_to be_present
+    end
+  end
+end
---
+++ spec/requests/channels_memberships_controller_spec.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+RSpec.describe Chat::Api::ChannelsMembershipsController do
+  fab!(:admin)
+  fab!(:moderator)
+  fab!(:other_user) { Fabricate(:user) }
+  fab!(:third_user) { Fabricate(:user) }
+  fab!(:public_category) { Fabricate(:category, name: "Public Category") }
+  fab!(:private_group) { Fabricate(:group, name: "whatsup") }
+  fab!(:private_category) { Fabricate(:category, name: "whatsup", read_restricted: true) }
+  fab!(:category_group) do
+    Fabricate(:category_group, category: private_category, group: private_group)
+  end
+  fab!(:public_channel) do
+    Fabricate(:category_channel)
+  end
+  fab!(:private_channel) do
+     Fabricate(:private_category_channel, group: private_group) 
+  end
+
+  before do
+    SiteSetting.chat_enabled = true
+    SiteSetting.chat_allowed_groups = Group::AUTO_GROUPS[:everyone]
+    private_channel.chatable_id = private_category.id
+    sign_in(admin)
+  end
+
+  describe "#create" do
+    describe "success" do
+      it "works for public channel" do
+        post "/chat/api/channels/#{public_channel.id}/memberships",
+             params: {
+               usernames: [other_user.username],
+             }
+
+        expect(response.status).to eq(200)
+
+        expect(Chat::UserChatChannelMembership.find_by(chat_channel_id: public_channel.id, following: true, user_id: other_user.id)).to be_present
+      end
+
+      it "works for private channel" do
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(0)
+
+        post "/chat/api/channels/#{private_channel.id}/memberships",
+              params: {
+                usernames: [other_user.username],
+              }
+
+        expect(response.status).to eq(200)
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(1)
+        expect(Chat::UserChatChannelMembership.find_by(chat_channel_id: private_channel.id, following: true, user_id: other_user.id)).to be_present
+        expect(GroupUser.find_by(group_id: Group.find_by(name:"whatsup").id, user_id: other_user.id)).to be_present
+      end
+
+
+      it "works for private channel even when you try to add same person again" do
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(0)
+
+        post "/chat/api/channels/#{private_channel.id}/memberships",
+              params: {
+                usernames: [other_user.username, third_user.username],
+              }
+
+        expect(response.status).to eq(200)
+
+        post "/chat/api/channels/#{private_channel.id}/memberships",
+              params: {
+                usernames: [third_user.username],
+              }
+
+        expect(response.status).to eq(200)
+
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(2)
+        expect(Chat::UserChatChannelMembership.find_by(chat_channel_id: private_channel.id, following: true, user_id: other_user.id)).to be_present
+        expect(GroupUser.find_by(group_id: Group.find_by(name:"whatsup").id, user_id: other_user.id)).to be_present
+      end
+
+
+      it "succeeds if the user is moderator" do
+        sign_in(moderator)
+        post "/chat/api/channels/#{public_channel.id}/memberships",
+              params: {
+                usernames: [other_user.username],
+              }
+
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+  
+  describe "failure" do
+    it "fails if the user is not staff" do
+      sign_in(third_user)
+      post "/chat/api/channels/#{public_channel.id}/memberships",
+            params: {
+              usernames: [other_user.username],
+            }
+
+      expect(response.status).to eq(422)
+    end
+  end
+
+
+  describe "#destroy" do
+    describe "success" do
+      it "works for public channel" do
+        post "/chat/api/channels/#{public_channel.id}/memberships",
+              params: {
+                usernames: [other_user.username],
+              }
+
+        expect(response.status).to eq(200)
+        delete "/chat/api/channels/#{public_channel.id}/memberships/#{other_user.username}"
+
+        expect(response.status).to eq(204)
+
+        expect(Chat::UserChatChannelMembership.find_by(chat_channel_id: public_channel.id, following: true, user_id: other_user.id)).to be_nil
+      end
+
+      it "works for private channel" do
+        post "/chat/api/channels/#{private_channel.id}/memberships",
+              params: {
+                usernames: [other_user.username],
+              }
+
+        expect(response.status).to eq(200)
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(1)
+
+        delete "/chat/api/channels/#{private_channel.id}/memberships/#{other_user.username}"
+
+        expect(response.status).to eq(204)
+
+        expect(GroupUser.where(group_id: CategoryGroup.find_by(category_id: private_channel.chatable.id).group_id).count).to eq(0)
+        expect(Chat::UserChatChannelMembership.find_by(chat_channel_id: private_channel.id, following: true, user_id: other_user.id)).to be_nil
+      end
+    end
+    describe "failure" do
+      it "fails if the user is not staff" do
+        sign_in(third_user)
+        delete "/chat/api/channels/#{public_channel.id}/memberships/#{other_user.username}"
+
+        expect(response.status).to eq(403)
+      end
+    end
+  end
+end
