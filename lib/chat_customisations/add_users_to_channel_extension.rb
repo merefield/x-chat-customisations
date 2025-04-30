2eb13cfaaac0a55b5ba12ef4f34a13a3681d3ea4
merefield <merefield@gmail.com>
2025-04-30 18:26:09 +0100

    FIX: allow users to add others to their group chat

--- lib/chat_customisations/add_users_to_channel_extension.rb
+++ lib/chat_customisations/add_users_to_channel_extension.rb
@@ -3,11 +3,10 @@ module ChatCustomisations
   module AddUsersToChannelExtension
 
     def can_add_users_to_channel(guardian:, channel:)
-      guardian.user.staff? &&
-      ((channel.direct_message_channel? && channel.chatable.group) ||
-      channel.chatable.is_a?(Category))
-      # (guardian.user.admin? || channel.joined_by?(guardian.user)) &&
-      #   channel.direct_message_channel? && channel.chatable.group
+      guardian.user.staff? ||
+      (channel.joined_by?(guardian.user) &&
+       channel.direct_message_channel? &&
+       channel.chatable.group)
     end
 
     def fetch_target_users(params:, channel:)
--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: x-chat-customisations
 # about: An extension to the Chat plugin that currently suppresses all emails when a user mentions @all
-# version: 0.0.21
+# version: 0.0.22
 # authors: Robert Barrow
 # url: https://github.com/merefield/x-chat-customisations
 
